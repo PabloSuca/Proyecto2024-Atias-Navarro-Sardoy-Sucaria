480d95be11539d4394413825d188823f40cba3e9
blasSARdoy <2318896@ucc.edu.ar>
2024-04-27 19:55:46 -0300

    Creación de grilla de juego con elementos div

--- SpaceInvaders/game.js
+++ SpaceInvaders/game.js
@@ -1,16 +1,26 @@
-//board
-let tileSize=32;
-let rows=16;
-let colums=16;
+const board = document.querySelector('.board')
+const boardtamaño = 24
 
-let board;
-let boardWidth=tileSize * columns; // 32*16
-let boardHeight=tileSize * rows; //32*16
-let context;
 
-window.onload = function() {
-    board = document.getElementById("board");
-    board.width = boardWidth;
-    board.height = boardHeight;
-    context = board.getContent("2d"); //used for drawing on the board
+
+
+//Se crean los divs que representan el fondo del juego, las casillas
+for( let i=0; i<boardtamaño*boardtamaño;i++){
+const square=document.createElement('div')
+board.appendChild(square)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
